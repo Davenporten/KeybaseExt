0e90e119db3b14f3c9ef66ae91aab56d64591d48
RClinton <davenporten@gmail.com>
2017-02-10 07:37:52 -0700

    Secured password a little more.

--- login.py
+++ login.py
@@ -12,7 +12,6 @@ except:
 import sys, json, scrypt, requests
 
 user = input('What is your username associated with Keybase: ')
-past = input('Keybase password (this cannot be accessed or stored outside of this script): ')
 js = json.loads(requests.get('https://keybase.io/_/api/1.0/getsalt.json?email_or_username='+user).text)
 
 if not js['status']['code'] is 0:
@@ -21,5 +20,5 @@ if not js['status']['code'] is 0:
 
 salt = js['salt']
 session = js['login_session']
-pass_phrase = scrypt.hash(past, salt, N=32768, r=8, p=1, buflen=256)
+pass_phrase = scrypt.hash(input('Keybase password (this cannot be accessed or stored outside of this script): '), salt, N=32768, r=8, p=1, buflen=256)
 print (pass_phrase)
