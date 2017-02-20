b6baf70fe100daee7a39806bcfc9782fb88eb4cf
RClinton <davenporten@gmail.com>
2017-02-20 12:44:52 -0700

    Added security for password input

--- login.py
+++ login.py
@@ -9,7 +9,7 @@ except:
     print ('The scrypt and requests modules are required, please install in your environment')
     quit()
 
-import sys, json, scrypt, requests
+import sys, json, scrypt, requests, getpass
 
 user = input('What is your username associated with Keybase: ')
 js = json.loads(requests.get('https://keybase.io/_/api/1.0/getsalt.json?email_or_username='+user).text)
@@ -20,7 +20,7 @@ if not js['status']['code'] is 0:
 
 salt = js['salt']
 session = js['login_session']
-pass_phrase = scrypt.hash(input('Keybase password (this cannot be accessed or stored outside of this script): '), salt, N=32768, r=8, p=1, buflen=256)
+pass_phrase = scrypt.hash(getpass.getpass('Keybase password (this cannot be accessed or stored outside of this script): '), salt, N=32768, r=8, p=1, buflen=256)
 
 v4 = pass_phrase[194:224]
 v5 = pass_phrase[224:256]
